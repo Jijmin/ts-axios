d9e2534e39322d90132bcd1dc88e96368ea695a4
周颖 <zhouying@zhouyingdeMacBook-Pro.local>
2020-03-22 14:13:44 +0800

    feat(all): ts-axios project init
    ts-axios 项目初始化，基础请求

---
+++ src/types/index.ts
@@ -0,0 +1,31 @@
+/**
+ * method 只能传入下面合法的字符串
+ */
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'Delete'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+
+/**
+ * url 为请求的地址，必选属性
+ * method 是请求的 HTTP 方法，可选属性
+ * data 是 post、patch 等类型请求的数据，放到 request body 中的，可选属性
+ * params 是 get、head 等类型请求的数据，拼接到 url 的 query string 中的，可选属性
+ */
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
