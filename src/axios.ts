9cf22becccd34a6668c973777bb7929028209ed1
周颖 <zhouying@zhouyingdeMacBook-Pro.local>
2020-03-24 12:50:06 +0800

    feat(all): extend axios.create
    扩展 axios.create 静态接口

--- src/axios.ts
+++ src/axios.ts
@@ -1,12 +1,13 @@
-import { AxiosInstance, AxiosRequestConfig } from './types'
+import { AxiosRequestConfig, AxiosStatic } from './types'
 import Axios from './core/Axios'
 import { extend } from './helpers/util'
 import defaults from './defaults'
+import mergeConfig from './core/mergeConfig'
 
 /**
  * 工厂函数创建 axios 实例
  */
-function createInstance(config: AxiosRequestConfig): AxiosInstance {
+function createInstance(config: AxiosRequestConfig): AxiosStatic {
   const context = new Axios(config)
   const instance = Axios.prototype.request.bind(context)
 
@@ -14,8 +15,12 @@ function createInstance(config: AxiosRequestConfig): AxiosInstance {
   // instance 本身是一个函数，又拥有了 Axios 类的所有原型和实例属性
   extend(instance, context)
 
-  return instance as AxiosInstance
+  return instance as AxiosStatic
 }
 
 const axios = createInstance(defaults)
+// 扩展 axios 的静态方法 create，可以通过合并的 config 创建新的实例
+axios.create = function create(config) {
+  return createInstance(mergeConfig(defaults, config))
+}
 export default axios
